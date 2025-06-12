89e2ce545e5c48760c69e69d9cab4388757aefe3
ZhouYongyou <128128010+zhouyongyou@users.noreply.github.com>
2025-06-12 20:20:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -90,12 +90,6 @@ function updateLanguage() {
   const referralLink = document.getElementById('referralLink');
   if (referralLink) referralLink.innerText = lang ? 'Your Referral Link:' : '推薦網址：';
 
-  const referralUrl = document.getElementById('referralUrl');
-  if (referralUrl) referralUrl.innerText = lang ? 'Referral URL:' : '你的推薦鏈接：';
-
-  const referralUrlSpan = document.getElementById('referralUrlSpan');
-  if (referralUrlSpan) referralUrlSpan.innerText = lang ? 'Referral URL:' : '你的推薦鏈接';
-
   const copyLinkBtn = document.getElementById('copyLinkBtn');
   if (copyLinkBtn) copyLinkBtn.innerText = lang ? 'Copy Link' : '複製鏈接';
   
