c599039f9bef5b9a5f33eb8786cc6f49b29325ee
ZhouYongyou <128128010+zhouyongyou@users.noreply.github.com>
2025-06-15 20:57:31 +0800

    Update token.sol

--- token.sol
+++ token.sol
@@ -32,6 +32,7 @@ interface IERC20 {
 contract Ownable is Context {
     address private _owner;
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipRenounced(address indexed previousOwner);
     constructor () {
         address msgSender = _msgSender();
         _owner = msgSender;
@@ -45,6 +46,7 @@ contract Ownable is Context {
         _;
     }
     function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipRenounced(_owner);
         emit OwnershipTransferred(_owner, address(0));
         _owner = address(0);
     }
@@ -75,7 +77,7 @@ interface IUniswapV2Router02 {
 }
 contract TokenDistributor {
     constructor (address token) {
-        IERC20(token).approve(msg.sender, uint(~uint256(0)));
+        IERC20(token).approve(msg.sender, type(uint256).max);
     }
 }
 
@@ -83,27 +85,30 @@ contract TOKEN is Context, IERC20, Ownable {
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
     mapping(address => bool) private _isExcludedFromFee;
-    address payable private _taxWallet;
+    address payable private immutable _taxWallet = payable(_msgSender());
     address public constant RouterAddress = address(0x10ED43C718714eb63d5aA57B78B54704E256024E);  // PancakeSwap: Router v2
-    address public constant USD1Address = address(0x8d0D000Ee44948FC98c9B98A4FA4921476f08B0d);  // USD1
-    uint256 public _buyFundFee = 100;
-    uint256 public _buyRewardFee = 300;
-    uint256 public _sellRewardFee = 300;
-    uint256 public _sellFundFee = 100;
+    address public constant _USD1 = address(0x8d0D000Ee44948FC98c9B98A4FA4921476f08B0d);  // USD1
+    uint256 public constant _buyFundFee = 100;
+    uint256 public constant _buyRewardFee = 300;
+    uint256 public constant _sellRewardFee = 300;
+    uint256 public constant _sellFundFee = 100;
     uint8 private constant _decimals = 9;
     uint256 private constant _tTotal = 1000000000000 * 10**_decimals;
     string private constant _name = unicode"BitLuck";
     string private constant _symbol = unicode"BTL";
-    uint256 public holderCondition;
+    uint256 public constant holderCondition = 200000000 * 10**_decimals; // 0.02%
     uint256 public drawIntervalBlocks = 2400; // 60 mins
     uint256 public lastDrawBlock;
-    IUniswapV2Router02 private uniswapV2Router;
-    address public _USD1;
+    IUniswapV2Router02 private immutable uniswapV2Router;
     mapping(address => bool) public _swapPairList;
-    uint256 private constant MAX = ~uint256(0);
-    TokenDistributor public _tokenDistributor;
+    uint256 private constant MAX = type(uint256).max;
+    TokenDistributor public immutable _tokenDistributor;
     uint256 public startTradeBlock;
-    bool private rewardEnabled= true;
+
+    uint256 private constant ACC_PRECISION = 1e18;
+    uint256 public totalDividendPerShare;
+    uint256 private accDividendBalance;
+    mapping(address => uint256) private lastDividendPerShare;
 
     bool private inSwap;
     modifier lockTheSwap {
@@ -112,13 +117,11 @@ contract TOKEN is Context, IERC20, Ownable {
         inSwap = false;
     }
     constructor () {
-        _taxWallet = payable(_msgSender());
         IUniswapV2Router02 swapRouter = IUniswapV2Router02(RouterAddress);
         uniswapV2Router = swapRouter;
-        _USD1 = USD1Address;
 
         IUniswapV2Factory swapFactory = IUniswapV2Factory(swapRouter.factory());
-        address swapPair = swapFactory.createPair(address(this), USD1Address);
+        address swapPair = swapFactory.createPair(address(this), _USD1);
         _swapPairList[swapPair] = true;
 
         _approve(address(this), address(swapRouter), MAX);
@@ -134,8 +137,6 @@ contract TOKEN is Context, IERC20, Ownable {
         _isExcludedFromFee[address(this)] = true;
         _isExcludedFromFee[address(swapRouter)] = true;
         _isExcludedFromFee[_taxWallet] = true;
-
-        holderCondition = _tTotal / 5000;
         _tokenDistributor = new TokenDistributor(_USD1);
     }
     function name() public pure returns (string memory) {
@@ -167,26 +168,28 @@ contract TOKEN is Context, IERC20, Ownable {
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
         _transfer(sender, recipient, amount);
         if (_allowances[sender][msg.sender] != MAX) {
-            _allowances[sender][msg.sender] = _allowances[sender][msg.sender] - amount;
+            unchecked {
+                _allowances[sender][msg.sender] = _allowances[sender][msg.sender] - amount;
+            }
         }
         return true;
     }
-    function _approve(address owner, address spender, uint256 amount) private {
+    function _approve(address owner, address spender, uint256 amount) internal {
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
-    function currentBlock() public view returns (uint256) {
-        return block.number;
+    function _updateRewards(address account) private {
+        uint256 owed = _balances[account] * (totalDividendPerShare - lastDividendPerShare[account]) / ACC_PRECISION;
+        if (owed > 0) {
+            safeTransfer(_USD1, account, owed);
+            accumulatedUsd1[account] += owed;
+            accDividendBalance -= owed;
+        }
+        lastDividendPerShare[account] = totalDividendPerShare;
     }
     function _transfer(address from, address to, uint256 amount) private {
         uint256 balance = balanceOf(from);
         require(balance >= amount, "balanceNotEnough");
-        if (!_isExcludedFromFee[from] && !_isExcludedFromFee[to]) {
-            uint256 maxSellAmount = balance * 999 / 1000;
-            if (amount > maxSellAmount) {
-                amount = maxSellAmount;
-            }
-        }
         bool takeFee;
         bool isSell;
         if (_swapPairList[from] || _swapPairList[to]) {
@@ -222,18 +225,19 @@ contract TOKEN is Context, IERC20, Ownable {
                 addHolder(from);
                 addHolder(to);
             }
-            if(rewardEnabled)
             processReward();
         }
     }
-    function _tokenTransfer(
+    function _tokenTransfer (
         address sender,
         address recipient,
         uint256 tAmount,
         bool takeFee,
         bool isSell
-    ) private {
-        _balances[sender] = _balances[sender] - tAmount;
+    ) internal {
+        unchecked {
+            _balances[sender] = _balances[sender] - tAmount;
+        }
         uint256 feeAmount;
         if (takeFee) {
             uint256 swapFee;
@@ -252,7 +256,9 @@ contract TOKEN is Context, IERC20, Ownable {
                 );
             }
         }
-        _takeTransfer(sender, recipient, tAmount - feeAmount);
+        unchecked {
+            _takeTransfer(sender, recipient, tAmount - feeAmount);
+        }
     }
     function swapTokenForFund(uint256 tokenAmount, uint256 swapFee) private lockTheSwap {
         address[] memory path = new address[](2);
@@ -276,7 +282,9 @@ contract TOKEN is Context, IERC20, Ownable {
         address to,
         uint256 tAmount
     ) private {
-        _balances[to] = _balances[to] + tAmount;
+        unchecked {
+            _balances[to] = _balances[to] + tAmount;
+        }
         emit Transfer(sender, to, tAmount);
     }
     function openTrading() external onlyOwner {
@@ -286,7 +294,6 @@ contract TOKEN is Context, IERC20, Ownable {
     }
     event USD1RewardDistributed(address indexed user, uint256 amount);
     mapping(address => uint256) public accumulatedUsd1;
-    // BNB deposit functionality removed
     address[] public holders;
     mapping(address => uint256) holderIndex;
     function addHolder(address adr) private {
@@ -305,53 +312,32 @@ contract TOKEN is Context, IERC20, Ownable {
             delete holderIndex[adr];
         }
     }
-    event USD1TransferFailed(address indexed to, uint256 amount);
     function processReward() private lockTheSwap {
-        if (!rewardEnabled) return;
         if (block.number < lastDrawBlock + drawIntervalBlocks) return;
-        if (holders.length == 0) return;
 
         IERC20 USD1 = IERC20(_USD1);
-        uint256 rewardPool = USD1.balanceOf(address(this));
-        if (rewardPool == 0) return;
+        uint256 balance = USD1.balanceOf(address(this));
+        uint256 available = balance > accDividendBalance ? balance - accDividendBalance : 0;
+        if (available == 0) return;
 
-        uint256 holderRewardPool = rewardPool / 40;
-        uint256 lotteryRewardPool = rewardPool / 120;
+        uint256 holderRewardPool = available / 40;
+        uint256 lotteryRewardPool = available / 120;
 
-        uint256 totalWeight = 0;
-        for (uint i = 0; i < holders.length; i++) {
-            totalWeight += balanceOf(holders[i]);
-        }
-
-        for (uint i = 0; i < holders.length; i++) {
-            address holder = holders[i];
-            uint256 holderReward = holderRewardPool * balanceOf(holder) / totalWeight;
-            if (holderReward > 0) {
-                (bool success, ) = _USD1.call(
-                    abi.encodeWithSelector(IERC20(_USD1).transfer.selector, holder, holderReward)
-                );
-                if (success) {
-                    accumulatedUsd1[holder] += holderReward;
-                } else {
-                    emit USD1TransferFailed(holder, holderReward);
-                }
+        if (holderRewardPool > 0) {
+            totalDividendPerShare += holderRewardPool * ACC_PRECISION / _tTotal;
+            accDividendBalance += holderRewardPool;
+            for (uint i = 0; i < holders.length; i++) {
+                address holder = holders[i];
+                _updateRewards(holder);
             }
         }
-
-        uint256 randIndex = random(0) % holders.length;
-        address winner = holders[randIndex];
-        uint256 lotteryReward = lotteryRewardPool;
-
-        if (lotteryReward > 0 && winner != address(0)) {
-            (bool successLottery, ) = _USD1.call(
-                abi.encodeWithSelector(IERC20(_USD1).transfer.selector, winner, lotteryReward)
-            );
-            if (successLottery) {
-                accumulatedUsd1[winner] += lotteryReward;
-                emit USD1RewardDistributed(winner, lotteryReward);
-            } else {
-                emit USD1TransferFailed(winner, lotteryReward);
-            }
+        // lottery draw
+        if (lotteryRewardPool > 0 && holders.length > 0) {
+            uint256 randIndex = random(0) % holders.length;
+            address winner = holders[randIndex];
+            safeTransfer(_USD1, winner, lotteryRewardPool);
+            accumulatedUsd1[winner] += lotteryRewardPool;
+            emit USD1RewardDistributed(winner, lotteryRewardPool);
         }
 
         lastDrawBlock = block.number;
@@ -369,10 +355,6 @@ contract TOKEN is Context, IERC20, Ownable {
                 )
             )
         );
-    }
-    function blocksUntilNextDraw() public view returns (uint256) {
-        if (block.number >= lastDrawBlock + drawIntervalBlocks) return 0;
-        return (lastDrawBlock + drawIntervalBlocks) - block.number;
     }
     function safeTransfer(address token, address to, uint256 value) internal {
         (bool success, bytes memory data) = token.call(
@@ -395,7 +377,16 @@ contract TOKEN is Context, IERC20, Ownable {
             safeTransfer(token, _taxWallet, toSend);
         }
     }
-    function getReferralCount(address referrer) external view returns (uint256) {
-        return referralCount[referrer];
+    fallback() external payable {
+    }
+    receive() external payable {
+    }
+    function blocksUntilNextDraw() public view returns (uint256) { // getUSD1RewardCountdown
+        if (block.number >= lastDrawBlock + drawIntervalBlocks) return 0;
+        return (lastDrawBlock + drawIntervalBlocks) - block.number;
+    }
+    function getUSD1Balance() external view returns (uint256) {
+        IERC20 usd1Token = IERC20(_USD1);
+        return usd1Token.balanceOf(address(this));
     }
 }
